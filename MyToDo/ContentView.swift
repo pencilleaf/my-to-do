f9ad7129f6e75b504935ce4c5347d3727529e25e
Claire <claire_tan@mymail.sutd.edu.sg>
2020-07-09 14:46:35 +0800

    Initial Commit

---
+++ MyToDo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyToDo
+//
+//  Created by loan on 9/7/20.
+//  Copyright © 2020 loan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
